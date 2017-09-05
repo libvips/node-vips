c89833ba9516eb4dcab0ebd897c99224d314c1f2
John Cupitt <jcupitt@gmail.com>
2017-09-05 19:06:34 +0100

    reorganise requires a bit

--- test/unit/gvalue.js
+++ test/unit/gvalue.js
@@ -1,7 +1,5 @@
 'use strict';
 
-console.log('hello from unit/gvalue.js');
-
 const fs = require('fs');
 const path = require('path');
 const assert = require('assert');
@@ -10,54 +8,54 @@ const vips = require('../../');
 
 describe('GValue', function () {
     it('Can make a GValue', function () {
-        var gv = new vips.gvalue.GValue()
+        var gv = new vips.GValue()
     });
 
     it('Can set/get a bool GValue', function () {
-        var gv = new vips.gvalue.GValue()
-        gv.init(vips.gvalue.GTYPE.gbool);
+        var gv = new vips.GValue()
+        gv.init(vips.GTYPES.gbool);
         gv.set(true);
         assert.strictEqual(gv.get(), true);
     });
 
     it('Can set/get a int GValue', function () {
-        var gv = new vips.gvalue.GValue()
-        gv.init(vips.gvalue.GTYPE.gint);
+        var gv = new vips.GValue()
+        gv.init(vips.GTYPES.gint);
         gv.set(12);
         assert.strictEqual(gv.get(), 12);
     });
 
     it('Can set/get a double GValue', function () {
-        var gv = new vips.gvalue.GValue()
-        gv.init(vips.gvalue.GTYPE.gdouble);
+        var gv = new vips.GValue()
+        gv.init(vips.GTYPES.gdouble);
         gv.set(3.1415);
         assert.strictEqual(gv.get(), 3.1415);
     });
 
     it('Can set/get a string GValue', function () {
-        var gv = new vips.gvalue.GValue()
-        gv.init(vips.gvalue.GTYPE.gstr);
+        var gv = new vips.GValue()
+        gv.init(vips.GTYPES.gstr);
         gv.set("hello world");
         assert.strictEqual(gv.get(), "hello world");
     });
 
     it('Can set/get a utf-8 string GValue', function () {
-        var gv = new vips.gvalue.GValue()
-        gv.init(vips.gvalue.GTYPE.gstr);
+        var gv = new vips.GValue()
+        gv.init(vips.GTYPES.gstr);
         gv.set("йцук");
         assert.strictEqual(gv.get(), "йцук");
     });
 
     it('Can set/get an array of int GValue', function () {
-        var gv = new vips.gvalue.GValue()
-        gv.init(vips.gvalue.GTYPE.array_int);
+        var gv = new vips.GValue()
+        gv.init(vips.GTYPES.array_int);
         gv.set([1, 2, 3]);
         assert.deepEqual(gv.get(), [1, 2, 3]);
     });
 
     it('Can set/get an array of double GValue', function () {
-        var gv = new vips.gvalue.GValue()
-        gv.init(vips.gvalue.GTYPE.array_double);
+        var gv = new vips.GValue()
+        gv.init(vips.GTYPES.array_double);
         gv.set([1.1, 2.1, 3.1]);
         assert.deepEqual(gv.get(), [1.1, 2.1, 3.1]);
     });
