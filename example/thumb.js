aa95f6fd973959a2f23dfa3f4b8d1d83c7ad9b51
John Cupitt <jcupitt@gmail.com>
2017-09-13 14:43:16 +0100

    simplify blob handling
    track object and blob counts for debugging

--- example/thumb.js
+++ example/thumb.js
@@ -1,13 +1,34 @@
 #!/usr/bin/env node
 
+/* 
+ * Thumbnail many images, either from a file source or memory buffer.
+ *
+ * It runs in a fairly steady 1gb of ram for me. Watching the output, you see
+ * stuff like:
+ *
+ * file processing /data/john/pics/sample/7350.jpg
+ * (2896 vips objects known to node-vips)
+ * file processing /data/john/pics/sample/7351.jpg
+ * (6 vips objects known to node-vips)
+ * file processing /data/john/pics/sample/7352.jpg
+ * (11 vips objects known to node-vips)
+ * file processing /data/john/pics/sample/7353.jpg
+ * (16 vips objects known to node-vips)
+ *
+ * So when around 3000 vips objects are alive, the node gc runs and they all get
+ * flushed. 
+ *
+ * If you want it to run in less ram than that, you'll need to expose the gc and
+ * trigger it manually every so often. 
+ */
+
 var fs = require('fs');
 var vips = require('..');
 
 // benchmark thumbnail via a memory buffer
 function via_memory(filename, thumbnail_width) {
-    console.log('memory processing', filename);
-
     var data = fs.readFileSync(filename);
+
     var thumb = vips.Image.thumbnail_buffer(data, thumbnail_width,
         {crop: 'centre'});
 
@@ -16,20 +37,19 @@ function via_memory(filename, thumbnail_width) {
 
 // benchmark thumbnail via files
 function via_files(filename, thumbnail_width) {
-    console.log('file processing', filename);
-
     var thumb = vips.Image.thumbnail(filename, thumbnail_width,
         {crop: 'centre'});
 
     return thumb.write_to_buffer('.jpg');
 }
 
-// disable the libvips operation cache .. it won't help for huge batch runs like
-// this
-vips.cache_set_max(0);
-
 for (var i = 2; i < process.argv.length; i++) {
-    // var thumb = via_memory(process.argv[i], 500)
-    var thumb = via_files(process.argv[i], 500)
+    var filename = process.argv[i];
+
+    console.log('processing' + filename + ', n_object = ' + vips.n_objects + 
+        ', g_free = ' + vips.n_g_free);
+    var thumb = via_memory(filename, 500)
+    // var thumb = via_files(filename, 500)
+
 }
 
